372b47db320b6707186346136f3556c0c51a099f
sasal22 <sasal.kross@gmail.com>
2024-01-22 17:52:59 +0600

    first commit

---
+++ js.js
@@ -0,0 +1,7 @@
+let burger = document.querySelector('.burger')
+let burgerBtn = document.querySelector(".burger__btn")
+console.log('fdhfgh');
+let toggleBurger = ()=>{
+    burger.classList.toggle('active')
+    burgerBtn.classList.toggle('active')
+}
